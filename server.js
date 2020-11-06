954caa78d4e849a0a1e978f7f6cd74c2830f7fd5
siddharthdodahatti <74051414+siddharthdodahatti@users.noreply.github.com>
2020-11-06 16:41:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const Articles = require('./models/article')
+const articleRouter = require('./routes/article')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog',{
+    useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+
+
+app.use(express.urlencoded({ extended:false}))
+app.use(methodOverride('_method'))
+
+app.get('/', async(req, res) => {
+   const articles = await Articles.find()
+     res.render('articles/index',{articles: articles})
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(8080)
